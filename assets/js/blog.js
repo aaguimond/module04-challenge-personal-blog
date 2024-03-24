f6a9d3195e3678b47a2e79f42320065bb6d197e3
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-03-23 20:36:22 -0500

    got JS working for storing blog posts in local storage and relocating to blog page when clicking submit!

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -14,3 +14,27 @@ toggleBtn.addEventListener('click', () => {
     } 
     localStorage.setItem('mode', mode);
 });
+
+const submissionForm = document.getElementById('submission-form')
+
+submissionForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const title = document.getElementById('blog-title').value;
+    const content = document.getElementById('blog-content').value;
+
+    const blogData = {
+        username: username,
+        title: title,
+        content: content
+    };
+
+    localStorage.setItem('blogData', JSON.stringify(blogData));
+
+    if (document.getElementById('username').value !== null &&
+        document.getElementById('blog-title').value !== null &&
+        document.getElementById('blog-content').value !== null) {
+            window.location.href = "index.html"
+    }
+})
